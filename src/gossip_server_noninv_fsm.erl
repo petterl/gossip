ab4eb83af13b83ff8730a1d6ec5519a760179303
Petter Sandholdt <petter@sandholdt.se>
2010-11-10 14:36:45 +0100

    First version of noninvite server fsm

---
+++ src/gossip_server_noninv_fsm.erl
@@ -0,0 +1,261 @@
+%% @author Petter Sandholdt <petter@sandholdt.se>
+%% @copyright 2010
+%% @doc NON-INVITE Server transaction FSM
+
+%%                                   |Request received
+%%                                   |pass to TU
+%%                                   V
+%%                             +-----------+
+%%                             |           |
+%%                             | Trying    |-------------+
+%%                             |           |             |
+%%                             +-----------+             |200-699 from TU
+%%                                   |                   |send response
+%%                                   |1xx from TU        |
+%%                                   |send response      |
+%%                                   |                   |
+%%                Request            V      1xx from TU  |
+%%                send response+-----------+send response|
+%%                    +--------|           |--------+    |
+%%                    |        | Proceeding|        |    |
+%%                    +------->|           |<-------+    |
+%%             +<--------------|           |             |
+%%             |Trnsprt Err    +-----------+             |
+%%             |Inform TU            |                   |
+%%             |                     |                   |
+%%             |                     |200-699 from TU    |
+%%             |                     |send response      |
+%%             |  Request            V                   |
+%%             |  send response+-----------+             |
+%%             |      +--------|           |             |
+%%             |      |        | Completed |<------------+
+%%             |      +------->|           |
+%%             +<--------------|           |
+%%             |Trnsprt Err    +-----------+
+%%             |Inform TU            |
+%%             |                     |Timer J fires
+%%             |                     |-
+%%             |                     |
+%%             |                     V
+%%             |               +-----------+
+%%             |               |           |
+%%             +-------------->| Terminated|
+%%                             |           |
+%%                             +-----------+
+%%
+%%                 Figure 8: non-INVITE server transaction
+-module(gossip_server_noninv_fsm).
+
+-behaviour(gen_fsm).
+-include_lib("gossip.hrl").
+-include_lib("esessin/src/stq.hrl").
+
+%% API
+-export([start_link/5, send/2, recv/2, transport_error/2]).
+
+%% gen_fsm callbacks
+-export([init/1, trying/2, proceeding/2, completed/2, handle_event/3,
+	 handle_sync_event/4, handle_info/3, terminate/3, code_change/4]).
+
+-record(state, { id :: transaction_id(), 
+		 con :: connection_info(), 
+		 resp_stq :: stq_opaque() | undefined,
+		 callback_modules = [] :: list(atom()),
+		 timer1 = 500 :: integer()}).
+
+%%====================================================================
+%% API
+%%====================================================================
+%% @doc Start an FSM for this transaction
+-spec start_link(transaction_id(), stq_opaque(), 
+		 connection_info(), list(atom()), Opts :: term()) -> 
+    {ok, pid()} | ignore | {error, Reason :: term()}.
+start_link(Id, STQ, Con, CallBackMods, Opts) ->
+    gen_fsm:start_link(?MODULE, {Id, STQ, Con, CallBackMods, Opts}, []).
+
+%% @doc Send an STQ to the network
+-spec send(pid(), stq_opaque()) -> ok.
+send(Pid, STQ) -> 
+    gen_fsm:send_event(Pid, {send, STQ}).
+    
+%% @doc Receive an STQ from the network
+-spec recv(pid(), stq_opaque()) -> ok.
+recv(Pid, STQ) -> 
+    gen_fsm:send_event(Pid, {recv, STQ}).
+
+%% @doc Receive an transport error from the network
+-spec transport_error(pid(), Reason :: term()) -> ok.
+transport_error(Pid, Reason) -> 
+    gen_fsm:send_all_state_event(Pid, {transport_error, Reason}).
+
+%%====================================================================
+%% gen_fsm callbacks
+%%====================================================================
+%% @doc Initial state, sends INVITE to user and moves to Proceeding state
+-spec init({transaction_id(), stq_opaque(), 
+	    connection_info(), Opts :: term()}) -> 
+    {ok, StateName :: atom(), State :: #state{}} |
+	{ok, StateName :: atom(), State :: #state{}, Timeout :: integer()} |
+	ignore | {stop, StopReason :: term()}.
+init({Id, STQ, Con, CallBackMods, Opts}) ->
+    Method = stq:method(STQ),
+    call(CallBackMods, Method, [Id, STQ, Con]),
+    DefaultState = #state{},
+    T1 = proplists:get_value(t1, Opts, DefaultState#state.timer1),
+    {ok, trying, #state{id = Id, con = Con, 
+			callback_modules = CallBackMods,
+			timer1 = T1}}.
+
+%% @doc Trying state, waiting for TU to send response to network, 
+%%      and move to Proceeding state
+-spec trying(Event :: term(), State :: #state{}) -> 
+    {next_state, NextStateName :: atom(), NextState :: #state{}} |
+	{next_state, NextStateName :: atom(), NextState :: #state{},
+	 Timeout :: integer()} |
+	{stop, Reason :: term(), NewState :: atom()}.
+trying({recv, _STQ}, State) ->
+    %% Ignore other messages receivied
+    {next_state, trying, State};
+trying({send, STQ}, State = #state{con = Con, timer1 = T1}) ->
+    %% TU sending an response to network, send it
+    gossip_transport:send(Con, STQ),
+    case stq:code(STQ) of
+	Code when Code >= 100, Code =< 199 -> 
+	    %% Provisional response, store and keep state
+	    {next_state, proceeding, State#state{resp_stq=STQ}};
+	Code when Code >= 200, Code =< 699 -> 
+	    %% Final response
+	    TJ = case reliable_transport(Con) of
+			 true -> 0;
+			 false -> 64 * T1
+		     end,
+	    gen_fsm:start_timer(TJ, timerJ),
+	    {next_state, completed, State#state{resp_stq=STQ}}
+    end.
+
+%% @doc Proceeding state, waiting for final response from TU
+%%      and move to Completed state
+-spec proceeding(Event :: term(), State :: #state{}) -> 
+    {next_state, NextStateName :: atom(), NextState :: #state{}} |
+	{next_state, NextStateName :: atom(), NextState :: #state{},
+	 Timeout :: integer()} |
+	{stop, Reason :: term(), NewState :: atom()}.
+proceeding({recv, _STQ}, State = #state{con = Con, resp_stq = RespSTQ}) ->
+    %% ReReceived request, resend last provisional response
+    gossip_transport:send(Con, RespSTQ),
+    {next_state, proceeding, State};
+proceeding({send, STQ}, State = #state{con = Con, timer1 = T1}) ->
+    %% TU sending an response to network, send it
+    gossip_transport:send(Con, STQ),
+    case stq:code(STQ) of
+	Code when Code >= 100, Code =< 199 -> 
+	    %% Provisional response, store and keep state
+	    {next_state, proceeding, State#state{resp_stq=STQ}};
+	Code when Code >= 200, Code =< 699 -> 
+	    TJ = case reliable_transport(Con) of
+			 true -> 0;
+			 false -> 64 * T1
+		     end,
+	    gen_fsm:start_timer(TJ, timerJ),
+	    {next_state, completed, State#state{resp_stq=STQ}}
+    end.
+
+%% @doc Completed state, waiting for ACK from network to move to Confirmed state
+-spec completed(Event :: term(), State :: #state{}) -> 
+    {next_state, NextStateName :: atom(), NextState :: #state{}} |         
+	{next_state,NextStateName :: atom(),NextState :: #state{},
+	 Timeout::integer()} |
+	{stop, Reason :: term(), NewState :: atom()}.
+completed({timeout, _Ref, timerJ}, State) ->
+    %% Terminate
+    {stop, normal, State};
+completed({recv, _STQ}, State = #state{con = Con, resp_stq = RespSTQ}) ->
+    %% ReReceived request, resend last response
+    gossip_transport:send(Con, RespSTQ),
+    {next_state, completed, State}.
+
+%%--------------------------------------------------------------------
+%% Function: 
+%% handle_event(Event, StateName, State) -> {next_state, NextStateName, 
+%%						  NextState} |
+%%                                          {next_state, NextStateName, 
+%%					          NextState, Timeout} |
+%%                                          {stop, Reason, NewState}
+%% Description: Whenever a gen_fsm receives an event sent using
+%% gen_fsm:send_all_state_event/2, this function is called to handle
+%% the event.
+%%--------------------------------------------------------------------
+handle_event({transport_error, Reason}, _StateName, 
+	     State = #state{id = Id, con=Con, 
+			    callback_modules = CallBackMods}) ->
+    %% TODO: Implement 3263 handling for retry sending to other server
+    %% Transport error from network, send to user
+    call(CallBackMods, transport_error, [Id, Reason, Con]),
+    {stop, normal, State}.
+
+%%--------------------------------------------------------------------
+%% Function: 
+%% handle_sync_event(Event, From, StateName, 
+%%                   State) -> {next_state, NextStateName, NextState} |
+%%                             {next_state, NextStateName, NextState, 
+%%                              Timeout} |
+%%                             {reply, Reply, NextStateName, NextState}|
+%%                             {reply, Reply, NextStateName, NextState, 
+%%                              Timeout} |
+%%                             {stop, Reason, NewState} |
+%%                             {stop, Reason, Reply, NewState}
+%% Description: Whenever a gen_fsm receives an event sent using
+%% gen_fsm:sync_send_all_state_event/2,3, this function is called to handle
+%% the event.
+%%--------------------------------------------------------------------
+handle_sync_event(_Event, _From, StateName, State) ->
+    Reply = ok,
+    {reply, Reply, StateName, State}.
+
+%%--------------------------------------------------------------------
+%% Function: 
+%% handle_info(Info,StateName,State)-> {next_state, NextStateName, NextState}|
+%%                                     {next_state, NextStateName, NextState, 
+%%                                       Timeout} |
+%%                                     {stop, Reason, NewState}
+%% Description: This function is called by a gen_fsm when it receives any
+%% other message than a synchronous or asynchronous event
+%% (or a system message).
+%%--------------------------------------------------------------------
+handle_info(_Info, StateName, State) ->
+    {next_state, StateName, State}.
+
+%%--------------------------------------------------------------------
+%% Function: terminate(Reason, StateName, State) -> void()
+%% Description:This function is called by a gen_fsm when it is about
+%% to terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_fsm terminates with
+%% Reason. The return value is ignored.
+%%--------------------------------------------------------------------
+terminate(_Reason, _StateName, _State) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% Function:
+%% code_change(OldVsn, StateName, State, Extra) -> {ok, StateName, NewState}
+%% Description: Convert process state when code is changed
+%%--------------------------------------------------------------------
+code_change(_OldVsn, StateName, State, _Extra) ->
+    {ok, StateName, State}.
+
+%%--------------------------------------------------------------------
+%%% Internal functions
+%%--------------------------------------------------------------------
+call([], _Fun, _Args) ->
+    ok;
+call([Mod | T], Fun, Args) ->
+    apply(Mod, Fun, Args),
+    call(T, Fun, Args).
+
+reliable_transport(Con) ->
+    case gossip_transport:type(Con) of
+	tcp ->
+	    true;
+	_ ->
+	    false
+    end.
---
+++ test/test_noninvite_server_fsm.erl
@@ -0,0 +1,345 @@
+%% -*- tab-width: 4;erlang-indent-level: 4;indent-tabs-mode: nil -*-
+
+-module(test_noninvite_server_fsm).
+
+-compile(export_all).
+
+-include_lib("eunit/include/eunit.hrl").
+
+-define(assertSend(Pid, Stq),
+        ?assertEqual(ok, gossip_server_noninv_fsm:send(Pid, Stq)),
+        ?assertMatch(Stq, recv(transport))).
+
+-define(assertRecv(Pid, Stq, Method),
+        ?assertEqual(ok, gossip_server_noninv_fsm:recv(Pid, Stq)),
+        ?assertMatch({_,Stq, _}, recv({application,Method}))).
+
+-define(assertDown(Pid),
+        erlang:monitor(process, Pid),
+        ?assertMatch({'DOWN',_,_,_,_}, recv()),
+        ?assertEqual(timeout, recv(1))).
+
+-define(assertResponse(Side, Code),
+        (fun() ->
+                 case recv(Side) of
+                     {_,Stq,_} ->
+                         ?assertEqual(response, stq:type(Stq)),
+                         ?assertEqual(Code, stq:code(Stq));
+                     Stq ->
+                         ?assertEqual(response, stq:type(Stq)),
+                         ?assertEqual(Code, stq:code(Stq))
+                 end
+         end)()).
+
+
+noninvite_test_() ->
+    {"non-Invite Tests",{setup, fun setup/0, fun teardown/1,
+     [{"REGISTER -> 200 -> TimerJ",fun sunny_day/0},
+      {"REGISTER -> 200 -> TimerJ (udp)",fun sunny_day_unreliable/0},
+      {"REGISTER -> 100 -> 200",fun wait_for_100/0},
+      {"REGISTER -> 100 -> REGISTER -> 100 -> 200",
+       fun wait_for_100_retransmit_reg/0},
+      {"REGISTER -> REGISTER -> 200", fun retransmit_reg/0},
+      {"REGISTER -> 100 -> TransErr", fun transport_error_provisional/0},
+      {"REGISTER -> 200 -> TransErr", fun transport_error_final_2xx/0},
+      {"BYE -> 200", fun method_bye/0},
+      {"CANCEL -> 200", fun method_cancel/0},
+      {"OPTION -> 200", fun method_option/0},
+      {"FOO -> 200", fun unknown_method/0}
+     ]}}.
+
+setup() ->
+    code:unstick_mod(gen_fsm),
+    MeckModules = [meck_module, gossip_transport, gen_fsm],
+    lists:map(fun(Mod) ->
+                      meck:new(Mod,[passthrough]),
+                      Mod
+              end, MeckModules).
+
+setup(Id, Stq, ConInfo, Opts) ->
+    Self = self(),
+    meck:expect(meck_module, register,
+                fun(MeckId, MeckStq, MeckConInfo) ->
+                        Self ! {{application,register},
+                                {MeckId, MeckStq, MeckConInfo}}
+                end),
+    meck:expect(meck_module, bye,
+                fun(MeckId, MeckStq, MeckConInfo) ->
+                        Self ! {{application,bye},
+                                {MeckId, MeckStq, MeckConInfo}}
+                end),
+    meck:expect(meck_module, option,
+                fun(MeckId, MeckStq, MeckConInfo) ->
+                        Self ! {{application,option},
+                                {MeckId, MeckStq, MeckConInfo}}
+                end),
+    meck:expect(meck_module, cancel,
+                fun(MeckId, MeckStq, MeckConInfo) ->
+                        Self ! {{application,cancel},
+                                {MeckId, MeckStq, MeckConInfo}}
+                end),
+    meck:expect(meck_module, foo,
+                fun(MeckId, MeckStq, MeckConInfo) ->
+                        Self ! {{application,foo},
+                                {MeckId, MeckStq, MeckConInfo}}
+                end),
+    meck:expect(meck_module, transport_error,
+                fun(MeckId, Reason, MeckConInfo) ->
+                        Self ! {{application,transport_error},
+                                {MeckId, Reason, MeckConInfo}}
+                end),
+    meck:expect(gossip_transport, send, fun(_Type, MeckStq) ->
+                                                Self ! {transport, MeckStq}
+                                        end),
+    meck:expect(gossip_transport, type, fun(Type) ->
+                                                Type
+                                        end),
+    meck:expect(gen_fsm, start_timer, fun(Val, Msg) ->
+                                              Ref = make_ref(),
+                                              Self ! {timer, {Val, Msg, Ref}},
+                                              Ref
+                                      end),
+    Res = (catch gossip_server_noninv_fsm:start_link(
+                   Id, Stq, ConInfo, [meck_module], Opts)),
+    ?assertMatch({ok, Pid} when is_pid(Pid), Res),
+    Res.
+
+teardown(_) ->
+    meck:unload(),
+    code:stick_mod(gen_fsm).
+
+sunny_day() ->
+    StqReg = stq:new(register, <<"sip:bob@localhost.com">>, {2,0}),
+    Stq200 = stq:new(200, <<"OK">>, {2,0}),
+    
+    {ok, Pid} = setup("id", StqReg, tcp, []),
+
+    %% Check that invite is sent to application
+    ?assertMatch({"id", StqReg, tcp}, recv({application,register})),
+
+    ?assertSend(Pid, Stq200),
+    TimerJ = recv(timer),
+    ?assertMatch({0,timerJ,_}, TimerJ),
+    {_,timerJ,Ref} = TimerJ,
+    %% Trigger timerJ
+    ?assertEqual(ok, gen_fsm:send_event(Pid, {timeout, Ref, timerJ})),
+    
+    ?assertDown(Pid).
+
+sunny_day_unreliable() ->
+    T1 = 10,
+    TJ = 64 * T1,
+    StqReg = stq:new(register, <<"sip:bob@localhost.com">>, {2,0}),
+    Stq200 = stq:new(200, <<"OK">>, {2,0}),
+    
+    {ok, Pid} = setup("id", StqReg, udp, [{t1, T1}]),
+
+    %% Check that invite is sent to application
+    ?assertMatch({"id", StqReg, udp}, recv({application,register})),
+
+    ?assertSend(Pid, Stq200),
+    TimerJ = recv(timer),
+    ?assertMatch({TJ,timerJ,_}, TimerJ),
+    {_,timerJ,Ref} = TimerJ,
+    %% Trigger timerJ
+    ?assertEqual(ok, gen_fsm:send_event(Pid, {timeout, Ref, timerJ})),
+    
+    ?assertDown(Pid).
+
+wait_for_100() ->    
+    StqReg = stq:new(register, <<"sip:bob@localhost.com">>, {2,0}),
+    Stq100 = stq:new(100, <<"Trying">>, {2,0}),
+    Stq200 = stq:new(200, <<"OK">>, {2,0}),
+    
+    {ok, Pid} = setup("id", StqReg, tcp, []),
+
+    %% Check that invite is sent to application
+    ?assertMatch({"id", StqReg, tcp}, recv({application,register})),
+
+    ?assertSend(Pid, Stq100),
+    ?assertSend(Pid, Stq200),
+    TimerJ = recv(timer),
+    ?assertMatch({0,timerJ,_}, TimerJ),
+    {_,timerJ,Ref} = TimerJ,
+    %% Trigger timerJ
+    ?assertEqual(ok, gen_fsm:send_event(Pid, {timeout, Ref, timerJ})),
+
+    ?assertDown(Pid).
+
+wait_for_100_retransmit_reg() ->
+    StqReg = stq:new(register, <<"sip:bob@localhost.com">>, {2,0}),
+    Stq100 = stq:new(100, <<"Trying">>, {2,0}),
+    Stq200 = stq:new(200, <<"OK">>, {2,0}),
+    
+    {ok, Pid} = setup("id", StqReg, tcp, []),
+
+    %% Check that invite is sent to application
+    ?assertMatch({"id", StqReg, tcp}, recv({application,register})),
+
+    ?assertSend(Pid, Stq100),
+
+    %% Retransmit Register
+    gossip_server_noninv_fsm:recv(Pid, StqReg),
+    ?assertMatch(Stq100, recv(transport)),
+    ?assertEqual(timeout, recv({application, register}, 10)),
+
+    ?assertSend(Pid, Stq200),
+    TimerJ = recv(timer),
+    ?assertMatch({0,timerJ,_}, TimerJ),
+    {_,timerJ,Ref} = TimerJ,
+    %% Trigger timerJ
+    ?assertEqual(ok, gen_fsm:send_event(Pid, {timeout, Ref, timerJ})),
+
+    ?assertDown(Pid).
+
+retransmit_reg() ->
+    StqReg = stq:new(register, <<"sip:bob@localhost.com">>, {2,0}),
+    Stq200 = stq:new(200, <<"OK">>, {2,0}),
+    
+    {ok, Pid} = setup("id", StqReg, tcp, []),
+
+    %% Check that invite is sent to application
+    ?assertMatch({"id", StqReg, tcp}, recv({application,register})),
+
+    %% Retransmit Register
+    gossip_server_noninv_fsm:recv(Pid, StqReg),
+    ?assertEqual(timeout, recv({application, register}, 10)),
+
+    ?assertSend(Pid, Stq200),
+    TimerJ = recv(timer),
+    ?assertMatch({0,timerJ,_}, TimerJ),
+    {_,timerJ,Ref} = TimerJ,
+    %% Trigger timerJ
+    ?assertEqual(ok, gen_fsm:send_event(Pid, {timeout, Ref, timerJ})),
+
+    ?assertDown(Pid).
+
+transport_error_provisional() ->
+    StqReg = stq:new(register, <<"sip:bob@localhost.com">>, {2,0}),
+    Stq100 = stq:new(100, <<"Trying">>, {2,0}),
+    
+    {ok, Pid} = setup("id", StqReg, tcp, []),
+
+    %% Check that invite is sent to application
+    ?assertMatch({"id", StqReg, tcp}, recv({application,register})),
+
+    ?assertSend(Pid, Stq100),
+
+    gossip_server_noninv_fsm:transport_error(Pid, fail),
+    ?assertMatch({_, fail, _}, recv({application, transport_error})),
+
+    ?assertDown(Pid).
+
+transport_error_final_2xx() ->
+    StqReg = stq:new(register, <<"sip:bob@localhost.com">>, {2,0}),
+    Stq200 = stq:new(200, <<"OK">>, {2,0}),
+    
+    {ok, Pid} = setup("id", StqReg, tcp, []),
+
+    %% Check that invite is sent to application
+    ?assertMatch({"id", StqReg, tcp}, recv({application,register})),
+
+    ?assertSend(Pid, Stq200),
+    ?assertMatch({_, timerJ, _}, recv(timer)),
+
+    gossip_server_noninv_fsm:transport_error(Pid, fail),
+    ?assertMatch({_, fail, _}, recv({application, transport_error})),
+
+    ?assertDown(Pid).
+
+method_bye() ->
+    Stq = stq:new(bye, <<"sip:bob@localhost.com">>, {2,0}),
+    Stq200 = stq:new(200, <<"OK">>, {2,0}),
+    
+    {ok, Pid} = setup("id", Stq, tcp, []),
+
+    %% Check that invite is sent to application
+    ?assertMatch({"id", Stq, tcp}, recv({application,bye})),
+
+    ?assertSend(Pid, Stq200),
+    TimerJ = recv(timer),
+    ?assertMatch({0,timerJ,_}, TimerJ),
+    {_,timerJ,Ref} = TimerJ,
+    %% Trigger timerJ
+    ?assertEqual(ok, gen_fsm:send_event(Pid, {timeout, Ref, timerJ})),
+    
+    ?assertDown(Pid).
+
+method_cancel() ->
+    Stq = stq:new(cancel, <<"sip:bob@localhost.com">>, {2,0}),
+    Stq200 = stq:new(200, <<"OK">>, {2,0}),
+    
+    {ok, Pid} = setup("id", Stq, tcp, []),
+
+    %% Check that invite is sent to application
+    ?assertMatch({"id", Stq, tcp}, recv({application,cancel})),
+
+    ?assertSend(Pid, Stq200),
+    TimerJ = recv(timer),
+    ?assertMatch({0,timerJ,_}, TimerJ),
+    {_,timerJ,Ref} = TimerJ,
+    %% Trigger timerJ
+    ?assertEqual(ok, gen_fsm:send_event(Pid, {timeout, Ref, timerJ})),
+    
+    ?assertDown(Pid).
+
+method_option() ->
+    Stq = stq:new(option, <<"sip:bob@localhost.com">>, {2,0}),
+    Stq200 = stq:new(200, <<"OK">>, {2,0}),
+    
+    {ok, Pid} = setup("id", Stq, tcp, []),
+
+    %% Check that invite is sent to application
+    ?assertMatch({"id", Stq, tcp}, recv({application,option})),
+
+    ?assertSend(Pid, Stq200),
+    TimerJ = recv(timer),
+    ?assertMatch({0,timerJ,_}, TimerJ),
+    {_,timerJ,Ref} = TimerJ,
+    %% Trigger timerJ
+    ?assertEqual(ok, gen_fsm:send_event(Pid, {timeout, Ref, timerJ})),
+    
+    ?assertDown(Pid).
+
+unknown_method() ->
+    Stq = stq:new(foo, <<"sip:bob@localhost.com">>, {2,0}),
+    Stq200 = stq:new(200, <<"OK">>, {2,0}),
+    
+    {ok, Pid} = setup("id", Stq, tcp, []),
+
+    %% Check that invite is sent to application
+    ?assertMatch({"id", Stq, tcp}, recv({application,foo})),
+
+    ?assertSend(Pid, Stq200),
+    TimerJ = recv(timer),
+    ?assertMatch({0,timerJ,_}, TimerJ),
+    {_,timerJ,Ref} = TimerJ,
+    %% Trigger timerJ
+    ?assertEqual(ok, gen_fsm:send_event(Pid, {timeout, Ref, timerJ})),
+    
+    ?assertDown(Pid).
+
+recv() ->
+    recv(250).
+recv(TO) when is_integer(TO) ->
+    receive
+        Msg ->
+            Msg
+    after TO ->
+            timeout
+    end;
+recv(Tag) ->
+    recv(Tag, 250).
+recv(Tag, TO) ->
+    receive
+        {Tag,Msg} ->
+            Msg
+    after TO ->
+            timeout
+    end.
+
+get_state(Pid) ->
+    {status,_Pid,_ModTpl, List} = sys:get_status(Pid),
+    io:format("~p",[List]),
+    AllData = lists:flatten([ X || {data,X} <- lists:last(List) ]),
+    proplists:get_value("StateData",AllData).
