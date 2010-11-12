8b7859597f9fada705827c75ca397fde267b8983
Petter Sandholdt <petter@sandholdt.se>
2010-11-12 21:21:31 +0100

    Add more tests to fulfill cover

--- test/test_noninvite_server_fsm.erl
+++ test/test_noninvite_server_fsm.erl
@@ -37,9 +37,12 @@ noninvite_test_() ->
      [{"REGISTER -> 200 -> TimerJ",fun sunny_day/0},
       {"REGISTER -> 200 -> TimerJ (udp)",fun sunny_day_unreliable/0},
       {"REGISTER -> 100 -> 200",fun wait_for_100/0},
+      {"REGISTER -> 100 -> 200 (udp)",fun wait_for_100_unreliable/0},
+      {"REGISTER -> 100 -> 100 -> 200",fun wait_for_100_retransmit_100/0},
       {"REGISTER -> 100 -> REGISTER -> 100 -> 200",
        fun wait_for_100_retransmit_reg/0},
       {"REGISTER -> REGISTER -> 200", fun retransmit_reg/0},
+      {"REGISTER -> 200 -> REGISTER -> 200", fun retransmit_reg_unreliable/0},
       {"REGISTER -> 100 -> TransErr", fun transport_error_provisional/0},
       {"REGISTER -> 200 -> TransErr", fun transport_error_final_2xx/0},
       {"BYE -> 200", fun method_bye/0},
@@ -166,6 +169,49 @@ wait_for_100() ->
 
     ?assertDown(Pid).
 
+wait_for_100_unreliable() ->    
+    T1 = 10,
+    TJ = 64 * T1,
+    StqReg = stq:new(register, <<"sip:bob@localhost.com">>, {2,0}),
+    Stq100 = stq:new(100, <<"Trying">>, {2,0}),
+    Stq200 = stq:new(200, <<"OK">>, {2,0}),
+    
+    {ok, Pid} = setup("id", StqReg, udp, [{t1, T1}]),
+
+    %% Check that invite is sent to application
+    ?assertMatch({"id", StqReg, udp}, recv({application,register})),
+
+    ?assertSend(Pid, Stq100),
+    ?assertSend(Pid, Stq200),
+    TimerJ = recv(timer),
+    ?assertMatch({TJ,timerJ,_}, TimerJ),
+    {_,timerJ,Ref} = TimerJ,
+    %% Trigger timerJ
+    ?assertEqual(ok, gen_fsm:send_event(Pid, {timeout, Ref, timerJ})),
+
+    ?assertDown(Pid).
+
+wait_for_100_retransmit_100() ->    
+    StqReg = stq:new(register, <<"sip:bob@localhost.com">>, {2,0}),
+    Stq100 = stq:new(100, <<"Trying">>, {2,0}),
+    Stq200 = stq:new(200, <<"OK">>, {2,0}),
+    
+    {ok, Pid} = setup("id", StqReg, tcp, []),
+
+    %% Check that invite is sent to application
+    ?assertMatch({"id", StqReg, tcp}, recv({application,register})),
+
+    ?assertSend(Pid, Stq100),
+    ?assertSend(Pid, Stq100),
+    ?assertSend(Pid, Stq200),
+    TimerJ = recv(timer),
+    ?assertMatch({0,timerJ,_}, TimerJ),
+    {_,timerJ,Ref} = TimerJ,
+    %% Trigger timerJ
+    ?assertEqual(ok, gen_fsm:send_event(Pid, {timeout, Ref, timerJ})),
+
+    ?assertDown(Pid).
+
 wait_for_100_retransmit_reg() ->
     StqReg = stq:new(register, <<"sip:bob@localhost.com">>, {2,0}),
     Stq100 = stq:new(100, <<"Trying">>, {2,0}),
@@ -209,6 +255,33 @@ retransmit_reg() ->
     TimerJ = recv(timer),
     ?assertMatch({0,timerJ,_}, TimerJ),
     {_,timerJ,Ref} = TimerJ,
+    %% Trigger timerJ
+    ?assertEqual(ok, gen_fsm:send_event(Pid, {timeout, Ref, timerJ})),
+
+    ?assertDown(Pid).
+
+retransmit_reg_unreliable() ->
+    T1 = 10,
+    TJ = 64 * T1,
+    StqReg = stq:new(register, <<"sip:bob@localhost.com">>, {2,0}),
+    Stq200 = stq:new(200, <<"OK">>, {2,0}),
+    
+    {ok, Pid} = setup("id", StqReg, udp, [{t1,T1}]),
+
+    %% Check that invite is sent to application
+    ?assertMatch({"id", StqReg, udp}, recv({application,register})),
+
+    ?assertSend(Pid, Stq200),
+
+    TimerJ = recv(timer),
+    ?assertMatch({TJ,timerJ,_}, TimerJ),
+    {_,timerJ,Ref} = TimerJ,
+
+    %% Retransmit Register
+    gossip_server_noninv_fsm:recv(Pid, StqReg),
+    ?assertMatch(Stq200, recv(transport)),
+    ?assertEqual(timeout, recv({application, register}, 10)),
+
     %% Trigger timerJ
     ?assertEqual(ok, gen_fsm:send_event(Pid, {timeout, Ref, timerJ})),
 
@@ -271,7 +344,7 @@ method_cancel() ->
     
     {ok, Pid} = setup("id", Stq, tcp, []),
 
-    %% Check that invite is sent to application
+    %% Check that cancel is sent to application
     ?assertMatch({"id", Stq, tcp}, recv({application,cancel})),
 
     ?assertSend(Pid, Stq200),
@@ -337,9 +410,3 @@ recv(Tag, TO) ->
     after TO ->
             timeout
     end.
-
-get_state(Pid) ->
-    {status,_Pid,_ModTpl, List} = sys:get_status(Pid),
-    io:format("~p",[List]),
-    AllData = lists:flatten([ X || {data,X} <- lists:last(List) ]),
-    proplists:get_value("StateData",AllData).
